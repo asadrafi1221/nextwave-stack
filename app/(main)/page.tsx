95921daba590da48d4011685c8dd7b4471e25304
asadrafi1221 <asadrafi1221@gmail.com>
2025-11-11 22:23:11 +0500

    cleanup(main/page.tsx): removed unused content inside return

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,57 +3,13 @@
 import { useUserStore } from "@/store/slices/user/store";
 import React from "react";
 import { Button, Input, Switch } from "@/components/global";
-import Tooltip from "@/components/global/Tooltip";
 import { Table } from "@/components/global/ThemeTable/Table";
 
 function Home() {
   const state = useUserStore((state) => state);
-  const { accessToken } = state;
   return (
     <>
-      <div className="bg-black text-white">
-        <p>Helo</p>
-        <Input placeholder="Email" />
-        <Button variant="default">Button</Button>
-        <Switch />
-      </div>
-      <div className="mt-20">
-        <Tooltip
-          content={"kina mara"}
-          side="bottom"
-          align="center"
-          animation="retro"
-        >
-          <p className="bg-red-900 text-white inline-block">
-            {"Ui Components Updated"}
-          </p>
-        </Tooltip>
-       
-
-        
-
-        <div className="px-44 mt-20 pb-20">
-          <Table
-            data={[
-              {
-                name: "John Doe",
-                age: 30,
-                email: "john.doe@example.com",
-              },
-              {
-                name: "Jane Doe",
-                age: 25,
-                email: "jane.doe@example.com",
-              },
-              {
-                name: "John Doe",
-                age: 30,
-                email: "john.doe@example.com",
-              },
-            ]}
-          />
-        </div>
-      </div>
+      In Progess ..... 
     </>
   );
 }
