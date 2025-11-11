5bd16968adc3a8ddc36eea41bd9b5382affca9ad
asadrafi1221 <asadrafi1221@gmail.com>
2025-11-11 22:23:33 +0500

    cleanup(main/page.tsx): removed unused content inside return

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { useUserStore } from "@/store/slices/user/store";
 import React from "react";
-import { Button, Input, Switch } from "@/components/global";
-import { Table } from "@/components/global/ThemeTable/Table";
 
 function Home() {
-  const state = useUserStore((state) => state);
   return (
     <>
       In Progess ..... 
